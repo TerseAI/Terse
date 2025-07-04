0bd8ca380b355fb99d03ea2e2f66ad636527ac83
Thomas Karatzas <thomasekaratzas@gmail.com>
2025-07-04 16:20:54 -0400

    Set up scripts

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import 'dotenv/config';
 import { createServer } from "http";
 import cors from 'cors';
+import { authMiddleware } from './routes/auth.js';
 const app = express();
 const server = createServer(app);
 app.use(cors({
@@ -12,8 +13,8 @@ app.use(cors({
 }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.get('/me', (req, res) => {
-    res.send('Hello World');
+app.get('/me', authMiddleware, (req, res) => {
+    res.send(req.session?.user);
 });
 server.listen(3001, () => {
     console.log('🚀 Express backend running on http://localhost:3001');
