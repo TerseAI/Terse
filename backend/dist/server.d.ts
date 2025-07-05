5f176a567ed7618e3367cf04798846357e4d86b8
Thomas Karatzas <thomasekaratzas@gmail.com>
2025-07-05 12:10:16 -0400

    Structured Search is working

--- backend/dist/server.d.ts
+++ backend/dist/server.d.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
-import { User } from './types/prisma.js';
-import { TicketManager } from './ticketing/TicketIntegration.js';
+import { User } from './types/prisma';
+import { User as TicketUser } from './shared/TicketSystem';
+import { TicketManager } from './ticketing/TicketIntegration';
 export type Session = {
     user: User;
     ticketManager?: TicketManager;
     isUserInitiated: boolean;
     teamId?: string;
+    currentUser?: TicketUser;
 };
 //# sourceMappingURL=server.d.ts.map
