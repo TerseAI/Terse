18b39070bf856e17ce260c18f5489964621323c8
Thomas Karatzas <thomasekaratzas@gmail.com>
2025-07-06 19:21:12 -0400

    Remove dist folder from git tracking - it should be ignored

--- backend/dist/shared/Entities.d.ts
+++
@@ -1,6 +0,0 @@
-export declare enum EntityType {
-    TICKET = "ticket",
-    COMMENT = "comment",
-    USER = "user"
-}
-//# sourceMappingURL=Entities.d.ts.map
--- backend/dist/shared/ModelEvents.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=ModelEvents.js.map
