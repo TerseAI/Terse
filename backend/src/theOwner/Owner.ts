63c5a5a432fe5bb1944c1495dcdfe0664a266630
Thomas Karatzas <thomasekaratzas@gmail.com>
2025-07-06 12:16:35 -0400

    Progressing Slack integration + fixed bug causing embeddings crashes

--- backend/src/theOwner/Owner.ts
+++ backend/src/theOwner/Owner.ts
@@ -20,19 +20,31 @@ class Owner {
         console.log('The owner is handling a push event', event);
         const analyzer = new Analyzer(this.session);
 
-        // Run semantic search on the push event
-        const searchResults = await this.searchSystem.search(pushEventToString(event), {
-            teamId: this.session.teamId || '',
-            entityTypes: ['ticket'],
-            minSimilarity: 0.4,
-            filters: [],
-            limit: 10
-        });
 
-        console.log(chalk.blue('Search results'), searchResults);
+        const results: SearchResult[][] = [];
+        for (const commitString of pushEventToString(event)) {
+            // Run semantic search on the push event
+            const searchResults = await this.searchSystem.search(commitString, {
+                teamId: this.session.teamId || '',
+                entityTypes: ['ticket'],
+                minSimilarity: 0.4,
+                filters: [],
+                limit: 10
+            });
+
+            console.log(chalk.blue('Search results for commit', commitString), searchResults);
+            results.push(searchResults);
+        }
+
+        // flatten the results
+        const flattenedResults = results.flat();
+        // de-duplicate the results. Anything with the same entityId and entityType reference the same entity
+        const uniqueResults = flattenedResults.filter((result, index, self) =>
+            index === self.findIndex((t) => t.entityId === result.entityId && t.entityType === result.entityType)
+        );
 
         // Run the analyzer
-        analyzer.analyze(eventForAgent(event, searchResults));
+        analyzer.analyze(eventForAgent(event, uniqueResults));
 
         // Run the analyzer
         const result = await analyzer.run();
@@ -60,7 +72,25 @@ export type FileDiff = {
 }
 
 // helper function to convert the push event to a string
-export const pushEventToString = (event: PushEvent) => {
+export const pushEventToString = (event: PushEvent): string[] => {
+
+    /// For every commit, we want to create a string that is a summary of the commit
+    const commitStrings = event.commits.map(commit => {
+        return `
+        username: ${event.username}
+        installationId: ${event.installationId}
+        repositoryName: ${event.repositoryName}
+        branch: ${event.branch}
+        commit: ${commit.name}
+        Changed Files: ${commit.fileDiffs.map(diff => diff.filename).join(', ')}
+        diffs: ${commit.fileDiffs.map(diff => diff.diff).join('\n')}
+        `;
+    });
+
+    return commitStrings;
+}
+
+export const eventForAgent = (event: PushEvent, searchResults: SearchResult[]): string => {
     return `
     username: ${event.username}
     installationId: ${event.installationId}
@@ -79,16 +109,16 @@ export const pushEventToString = (event: PushEvent) => {
     `;
 }
 
-export const eventForAgent = (event: PushEvent, searchResults: SearchResult[]): string => {
-    let eventString = pushEventToString(event);
+// export const eventForAgent = (event: PushEvent, searchResults: SearchResult[]): string => {
+//     let eventString = pushEventToString(event);
 
-    eventString += `
-    Possibly Related Tickets:
-    `;
+//     eventString += `
+//     Possibly Related Tickets:
+//     `;
 
-    eventString += searchResults.map(result => `
-    ${result.content}
-    `).join('\n');
+//     eventString += searchResults.map(result => `
+//     ${result.content}
+//     `).join('\n');
 
-    return eventString;
-}
+//     return eventString;
+// }
