1fb125b3a9c30cb5ba0b967a5243a988b4549e38
thomas-terse <thomas@useterse.ai>
2025-07-11 10:00:24 -0400

    Good first take for landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,13 +5,15 @@ import Spin from "./components/Spin";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import { IntegrationProvider } from "./context/Integrations";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Content />} />
+          <Route path="/app" element={<Content />} />
+          <Route path="/" element={<LandingPage />} />
         </Routes>
       </Router>
     </AuthProvider>
