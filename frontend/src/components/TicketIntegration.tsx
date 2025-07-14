6439e8b7fd2382cd84ba79e7ff33361e128bacac
Thomas Karatzas <thomasekaratzas@gmail.com>
2025-07-13 22:54:15 -0400

    Simplified some code in the front end

--- frontend/src/components/TicketIntegration.tsx
+++ frontend/src/components/TicketIntegration.tsx
@@ -1,22 +1,9 @@
 import { AddLinear } from "./AddLinear";
-import { useIntegrations } from "../context/Integrations";
 
 interface TicketIntegrationProps {
     onIntegrationChange: () => Promise<void>;
 }
 
 export function TicketIntegration({ onIntegrationChange }: TicketIntegrationProps) {
-    const { hasLinear, hasJira } = useIntegrations();
-
-    if (!hasLinear && !hasJira) {
-        return (
-            <div className="space-y-3">
-                <AddLinear onIntegrationChange={onIntegrationChange} />
-            </div>
-        )
-    }
-
-    else {
-        return <AddLinear onIntegrationChange={onIntegrationChange} />
-    }
+    return <AddLinear onIntegrationChange={onIntegrationChange} />
 } 
