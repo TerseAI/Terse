1573060b36a9fbabc81bacd404da4a6183485398
Thomas Karatzas <thomasekaratzas@gmail.com>
2025-08-27 14:18:47 -0400

    Small UI change to test the system

--- frontend/src/components/activity/AvatarBar.tsx
+++ frontend/src/components/activity/AvatarBar.tsx
@@ -6,7 +6,7 @@ function AvatarBar({ event }: { event: ActivityEvent }) {
     return (
         <div className="flex items-center gap-3">
             <GitHubAvatar username={event.github_repository_owner_id} size={48} />
-            <div className="grid auto-cols-max grid-flow-col gap-2">
+            <div className="grid auto-cols-max grid-flow-col gap-3">
                 <h3 className="font-semibold text-lg text-[theme(text-primary)]">
                     {event.github_repository_name}
                 </h3>
