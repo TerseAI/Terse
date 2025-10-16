6f27ce1068cbbe94f725eba5be1953a4a633f0fa
Thomas Karatzas <thomasekaratzas@gmail.com>
2025-10-16 16:13:33 -0400

    Added some styling

--- frontend/src/pages/Automations/components/SectionLayout.tsx
+++ frontend/src/pages/Automations/components/SectionLayout.tsx
@@ -7,7 +7,7 @@ interface SectionLayoutProps {
 
 export function SectionLayout({ title, children }: SectionLayoutProps) {
     return (
-        <div className="relative flex justify-center">
+        <div className="relative flex justify-center items-center">
             <h1 className="absolute left-0 text-lg font-bold text-[theme(text-primary)]">{title}</h1>
             <div className="grid grid-flow-col gap-4">
                 {children}
