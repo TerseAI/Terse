d841d15f2dde49c9842f71228a4ddf4d33df7bd9
thomas-terse <thomas@useterse.ai>
2025-10-15 23:27:28 -0400

    Decent progress again. It's getting the point across

--- frontend/src/pages/Automations/InputSection.tsx
+++ frontend/src/pages/Automations/InputSection.tsx
@@ -1,21 +1,18 @@
 import { Input, useAutomationContext } from "../../context/AutomationContext";
+import { IntegrationBox, IntegrationInput } from "./components/Integration";
+import { SectionLayout } from "./components/SectionLayout";
 
 export function InputsSection() {
     const { inputs, setInputs } = useAutomationContext();
 
 
     return (
-        <div className="relative flex justify-center">
-            <h1 className="absolute left-0 text-lg font-bold text-[theme(text-primary)]">Using Information From</h1>
-            <div className="grid grid-flow-col gap-4">
-                {inputs.map((input) => (
-                    <div key={input.name}>
-                        <h1 className="text-lg font-bold text-[theme(text-primary)]">{input.name}</h1>
-                    </div>
-                ))}
-                <AddInputButton />
-            </div>
-        </div>
+        <SectionLayout title="Using Information From">
+            {inputs.map((input) => (
+                <IntegrationInput key={input} input={input} />
+            ))}
+            <AddInputButton />
+        </SectionLayout>
     )
 }
 
@@ -28,15 +25,14 @@ export function AddInputButton() {
     }
 
     return (
-        <div className="grid grid-flow-col gap-4">
-            <button 
+        <IntegrationBox>
+            <button
                 onClick={handleAddInput}
-                className="w-12 h-12 border-2 border-[theme(text-primary)] border-solid flex items-center justify-center p-3"
             >
                 <svg className="w-6 h-6 text-[theme(text-primary)]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
                 </svg>
             </button>
-        </div>
+        </IntegrationBox>
     )
 }
