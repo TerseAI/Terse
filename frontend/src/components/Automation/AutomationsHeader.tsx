227f1d3d95da10ac3986f9b5e60edeca48b72466
Thomas Karatzas <thomasekaratzas@gmail.com>
2025-10-30 16:20:28 -0400

    Support for light mode + fixed some small issues

--- frontend/src/components/Automation/AutomationsHeader.tsx
+++ frontend/src/components/Automation/AutomationsHeader.tsx
@@ -7,8 +7,8 @@ interface AutomationsHeaderProps {
 
 export function AutomationsHeader({ onCreateNew }: AutomationsHeaderProps) {
     return (
-        <div className="grid grid-cols-20 sm:grid-flow-row sm:items-center sm:justify-between gap-4">
-            <h1 className="text-xl font-bold text-white col-span-16">Automations</h1>
+        <div className="grid grid-cols-20 sm:grid-flow-row sm:items-center sm:justify-between gap-4 pt-1">
+            <h1 className="text-xl font-bold text-[theme(text-primary)] col-span-16">Automations</h1>
             <Button
                 onClick={onCreateNew}
                 className="col-span-4 inline-flex items-center justify-center text-sm font-medium text-white transition-colors"
