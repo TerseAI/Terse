99a56ec5b19fa4605f609cadce403dd761b73500
Olivier Morissette <olivier@useterse.ai>
2025-11-14 15:41:12 -0500

    fix bug, remove w-full in notion resources

--- frontend/src/components/NotionResourceSelector.tsx
+++ frontend/src/components/NotionResourceSelector.tsx
@@ -162,7 +162,7 @@ function NotionResourceCombobox({
                     <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                 </Button>
             </PopoverTrigger>
-            <PopoverContent className="w-full p-0" align="start">
+            <PopoverContent className="p-0" align="start">
                 <Command>
                     <CommandInput placeholder="Search pages or databases..." />
                     <CommandList>
