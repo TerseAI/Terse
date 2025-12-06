6cb64006f59cdfd9aa50412aacf8ddc3bc9ac15c
Oli Simard-Morissette <olivier@useterse.ai>
2025-12-06 11:11:02 -0500

    [TER-151] - Fix Notion Workspace Selection Bug (#102)
    * Fix bug with selecting Notion db or page
    
    * fix multiple integrations not working well
    
    * make placeholder optional, set reasonable default

--- frontend/src/components/ui/DropdownSelect.tsx
+++ frontend/src/components/ui/DropdownSelect.tsx
@@ -16,26 +16,27 @@ export type StatusOption = {
 
 type DropdownSelectProps = {
     statusOptions: StatusOption[];
-    selectedOption: StatusOption;
+    selectedOption: StatusOption | null;
     setSelected: (value: string) => void;
     additionalAction?: {
         label: string;
         onClick: () => void;
     };
+    placeholder?: string;
 }
 
-const DropdownSelect = ({ statusOptions, selectedOption, setSelected, additionalAction }: DropdownSelectProps) => {
+const DropdownSelect = ({ statusOptions, selectedOption, setSelected, additionalAction, placeholder = 'Select an option' }: DropdownSelectProps) => {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="outline">
-                    <span className="block truncate">{selectedOption.label}</span>
+                    <span className="block truncate">{selectedOption?.label ?? placeholder}</span>
                     <ChevronDown className="h-4 w-4 ml-2 text-muted-foreground shrink-0" />
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="max-h-60" align="start">
                 <DropdownMenuRadioGroup
-                    value={selectedOption.value}
+                    value={selectedOption?.value ?? ''}
                     onValueChange={setSelected}
                 >
                     {statusOptions.map((option, idx) => (
