58ed866a241e8e9daff5c46b6953cb29a1c4511f
Olivier Morissette <olivier@useterse.ai>
2026-01-19 13:44:39 -0500

    fix migration scripts

--- backend/prisma/migrations/20260119184433_multiple_outputs/migration.sql
+++ backend/prisma/migrations/20260119184433_multiple_outputs/migration.sql
@@ -4,8 +4,5 @@ ALTER TYPE "OutputConfigType" ADD VALUE 'GMAIL';
 -- DropIndex
 DROP INDEX "automation_outputs_automation_id_key";
 
--- AlterTable
-ALTER TABLE "automation_slack_configs" ADD COLUMN     "acknowledge_with_emoji" BOOLEAN NOT NULL DEFAULT false;
-
 -- CreateIndex
 CREATE INDEX "automation_outputs_automation_id_idx" ON "automation_outputs"("automation_id");
