d22fbdbfe0a7f48b88129a07dd9f1e913555c54d
Thomas Karatzas <thomasekaratzas@gmail.com>
2026-01-26 10:42:34 -0500

    Wiring almost done

--- backend/src/agent/ChatAgent/ChatInterfaces/WebChatInterface.ts
+++ backend/src/agent/ChatAgent/ChatInterfaces/WebChatInterface.ts
@@ -2,9 +2,12 @@ import { RunStreamEvent } from "@openai/agents-core";
 import { ConfigType } from "../../../shared/Configs";
 import { IntegrationType } from "../../../shared/Integrations";
 import ChatInterface from "./ChatInterface";
+import { Socket } from "socket.io";
 
 class WebChatInterface extends ChatInterface {
     name: string = 'Web';
+    private readonly socket: Socket;
+    
     promptForIntegration(integration: IntegrationType): Promise<string> {
         throw new Error("Method not implemented.");
     }
@@ -20,7 +23,10 @@ class WebChatInterface extends ChatInterface {
     buildButton(label: string, url: string): Promise<void> {
         throw new Error("Method not implemented.");
     }
-    constructor(sessionId: string, userId: string) {
+    constructor(sessionId: string, userId: string, socket: Socket) {
         super(sessionId, userId);
+        this.socket = socket;
     }
 }
+
+export default WebChatInterface;
