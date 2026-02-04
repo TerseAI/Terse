715f5fecd01780030f0c3c45e1e73e398eff9eeb
Thomas Karatzas <thomasekaratzas@gmail.com>
2026-02-04 13:46:10 -0500

    Remove prettier

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env zsh
 set -e
 
-# Source shell profile to get proper PATH (nvm, fnm, volta, etc.)
-if [[ -f "$HOME/.zshrc" ]]; then
-  source "$HOME/.zshrc"
-fi
-
-# Run Linter
-
-cd backend && pnpm run format && cd ..
-cd frontend && pnpm run format && cd ..
-
 # Run build in backend
 pnpm --prefix backend run build
 
