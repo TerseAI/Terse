0ac9e6bad257ab351692ac59f3879f01cea5588c
Thomas Karatzas <thomasekaratzas@gmail.com>
2026-02-18 13:57:47 -0500

    More cleanup

--- frontend/src/components/RunHistory/RunHistoryChatDrawer/index.tsx
+++ frontend/src/components/RunHistory/RunHistoryChatDrawer/index.tsx
@@ -3,7 +3,7 @@ import { useRef, useState } from "react"
 import { Chat, type ChatHandle } from "@/components/chat/Chat"
 import { Drawer, DrawerContent } from "@/components/ui/drawer"
 import { cn } from "@/lib/utils"
-import { RunHistoryRecord, RunHistoryStatus, RunHistoryTrigger } from "@/shared/RunHistoryTypes"
+import { RunHistoryRecord, RunHistoryStatus } from "@/shared/RunHistoryTypes"
 
 import RunHistoryChatAdapter from "./RunHistoryChatAdapter"
 import RunHistoryChatDrawerHeader from "./RunHistoryChatDrawerHeader"
