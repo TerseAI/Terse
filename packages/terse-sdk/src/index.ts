d28674fd83c301d54dca1b3b97c5c91b8bf0bba9
thomas-terse <thomas@useterse.ai>
2026-02-24 17:21:28 -0500

    Removed sample code

--- packages/terse-sdk/src/index.ts
+++ packages/terse-sdk/src/index.ts
@@ -4,7 +4,6 @@ import type { InputEvent, ToolboxEntry } from "./types.js"
 import type { ConfigInstance } from "./shared/Configs.js"
 import type { RunHistoryAction } from "./shared/RunHistoryTypes.js"
 import { IntegrationType } from "./shared/Integrations.js"
-
 // Re-export SDK-specific types
 export type { InputEvent, ToolboxEntry } from "./types.js"
 
@@ -186,48 +185,3 @@ export class ActionResult implements TerseAgentResult {
         this.action = action
     }
 }
-
-
-/**
- * CODE GENERATION CODE: This is user specific!!!
- */
-
-// class GithubRepositories {
-//     constructor(public readonly repositoryId: number) {}
-
-//     // This is Code-Gened Pre-Compiled step
-//     static TerseAI = new GithubRepositories(10030021)
-//     static LandingPage = new GithubRepositories(10030022)
-// }
-
-// class GithubSkill extends GitHubConfigInternal {
-//     constructor(repositories: GithubRepositories[]) {
-//         const repositoryIds: number[] = repositories.map(r => r.repositoryId)
-//         // Github token is
-//         super("github_token", repositoryIds)
-//     }
-// }
-
-/**
- * CLIENT CODE: What the user will write
- */
-
-// const client = new Terse()
-
-// // This closure will be run according to the trigger configuration in a sandboxed environment.
-// const ReleaseNotesJob = client.createJob({
-//     name: "Release Notes Job",
-//     triggers: [new TimeTrigger("0 0 * * *")],
-//     skills: [new GithubSkill([GithubRepositories.TerseAI, GithubRepositories.LandingPage])],
-//     onTrigger: async (event: InputEvent, Agent) => {
-//         const prompt = "You are a helpful assistant that can help with the release notes"
-
-//         // Run any code here. Can use the toolbox, and our TerseAgent, or they can do whatever they want.
-//         const terseAgent = Agent.run(prompt, event)
-//     },
-//     webhookURL: "https://example.com/webhook"
-// })
-
-/**
- * From here, we can test, deploy debug with either CLI and trigger with GithubActions. We can still expose these in WebUI to analyze runs and stuff.
- */
