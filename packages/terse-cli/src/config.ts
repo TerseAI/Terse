5e5274adcb25e7dadc45adb6c38ecebe1531dd7c
Olivier Morissette <olivier@useterse.ai>
2026-03-18 20:34:19 -0400

    Fix URLs in prod

---
+++ packages/terse-cli/src/config.ts
@@ -0,0 +1,2 @@
+export const FRONTEND_URL = process.env.TERSE_FRONTEND_URL || "https://app.useterse.ai"
+export const BACKEND_URL = process.env.TERSE_BACKEND_URL || "https://cursor-for-tickets.onrender.com"
