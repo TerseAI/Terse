27e0bfa572477a0650f997078a161040bf8eea24
Thomas Karatzas <thomas@useterse.ai>
2026-03-30 14:03:59 -0400

    Works super well (#413)

--- packages/terse-cli/src/config.ts
+++ packages/terse-cli/src/config.ts
@@ -1,2 +1,3 @@
 export const FRONTEND_URL = process.env.TERSE_FRONTEND_URL || "https://app.useterse.ai"
 export const BACKEND_URL = process.env.TERSE_BACKEND_URL || "https://cursor-for-tickets.onrender.com"
+export const WORKOS_CLIENT_ID = "client_01KG311KCS20M8VF1KK8F76NJE"
