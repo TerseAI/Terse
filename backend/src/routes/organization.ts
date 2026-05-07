3d0ea57d51bfd862e74fc2f906074d7d6bba7f9b
Olivier Morissette <olivier@useterse.ai>
2026-05-07 17:44:55 -0400

    workos id pelase

--- backend/src/routes/organization.ts
+++ backend/src/routes/organization.ts
@@ -35,7 +35,7 @@ export async function createOrganization(req: Request, res: Response) {
             roleSlug: "admin"
         })
 
-        const billing = billingServiceProxyForOrganization(organization.id, user.id)
+        const billing = billingServiceProxyForOrganization(organization.id, user.workosId)
         const { customerId } = await billing.getOrCreateCustomer()
         await setDefaultOrganizationMetadata(organization.id, customerId)
 
