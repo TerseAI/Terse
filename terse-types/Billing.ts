e1ddb18f208eaed37b4f59db0428bdd012561171
Olivier Morissette <olivier@useterse.ai>
2026-05-07 17:37:06 -0400

    fix user id being passed to backend

--- terse-types/Billing.ts
+++ terse-types/Billing.ts
@@ -262,7 +262,7 @@ export const BILLING_SERVICE_CALLBACK_JWT_AUDIENCE = "terse-api" as const
 
 export const terseBillingJwtClaimsSchema = z.object({
     organizationId: z.string().min(1),
-    userId: z.string().min(1)
+    userId: z.string().min(1).optional()
 })
 export type TerseBillingJwtClaims = z.infer<typeof terseBillingJwtClaimsSchema>
 
