3b2258db7b03b337301bb29fe0b3b70bb4f5062d
Thomas Karatzas <thomasekaratzas@gmail.com>
2026-08-15 15:39:59 -0400

    Fixed theme issue

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -2,12 +2,14 @@ import { ReactNode } from "react"
 
 import { ThemeProvider } from "next-themes"
 
+import { ThemeColorMeta } from "@/components/ThemeColorMeta"
 import { Toaster } from "@/components/ui/sonner"
 import { AuthProvider } from "@/modules/auth/context/AuthProvider"
 
 export function Providers({ children }: { children: ReactNode }) {
     return (
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem storageKey="vite-ui-theme" disableTransitionOnChange>
+            <ThemeColorMeta />
             <Toaster position="top-center" richColors={true} />
             <AuthProvider>{children}</AuthProvider>
         </ThemeProvider>
